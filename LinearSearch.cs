09adfad5fe1937a3e572479d89d1a15b0daa920c
atiyeh85 <56246156+atiyeh85@users.noreply.github.com>
2023-03-14 09:52:53 -0600

    Add files via upload

---
+++ LinearSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class LinearSearch
+    {
+        public static int Linear_Search(int x, int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                    if (x == nums[i])
+                    {
+                        return i;
+                    }
+            }
+
+            return 0;
+        }
+    }
+}
